5c272280698eccaf52ec0013244fb0d67d90ecc7
Sahil Ramjeeth <22046468@dut4life.ac.za>
2026-01-08 12:41:46 +0200

    Fix B

---
+++ docs/_app/immutable/chunks/BPMJ6W4n.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1hbz6n1?.base??"/jwt-viz",t=globalThis.__sveltekit_1hbz6n1?.assets??s??"";export{t as a,s as b};
--- docs/_app/immutable/chunks/CNV_aR6h.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_n4au9f?.base??"/jwt-viz",a=globalThis.__sveltekit_n4au9f?.assets??s??"";export{a,s as b};
